af8b5bf825ccbf29845e9ba8d75e369ac6d26a60
mauricio.fontana <113035089+mauricioFontanaEa@users.noreply.github.com>
2023-02-13 19:35:03 -0300

    started project with base time elapsed screen with accessibility labels

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mauricio Fontana on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
